130a6bebe879f84c4434527e13c0d3b8288776fc
Cristobal Vasquez <cristobal.vasquez.m@usach.cl>
2024-11-07 17:33:31 -0300

    added docker setup

--- src/routes.ts
+++ src/routes.ts
@@ -2,45 +2,45 @@ import { MainController } from "./controller/MainController"
 
 export const Routes = [{
     method: "get",
-    route: "/catalogue",
+    route: "/api/v1/catalogue",
     controller: MainController,
     action: "storeHasFoodAll"
 }, {
     method: "get",
-    route: "/catalogue/bystore/:id",
+    route: "/api/v1/catalogue/bystore/:id",
     controller: MainController,
     action: "storeHasFoodOneByStore"
 }, {
     method: "post",
-    route: "/catalogue",
+    route: "/api/v1/catalogue",
     controller: MainController,
     action: "storeHasFoodSave"
 }, {
     method: "delete",
-    route: "/catalogue/bystoreandfood/:storeId/:foodLocalId",
+    route: "/api/v1/catalogue/bystoreandfood/:storeId/:foodLocalId",
     controller: MainController,
     action: "storeHasFoodRemove"
 }, {
     method: "patch",
-    route: "/catalogue/bystore/:id",
+    route: "/api/v1/catalogue/bystore/:id",
     controller: MainController,
     action: "storeHasFoodUpdate"
 },
 {
     method: "get",
-    route: "/store-profile",
+    route: "/api/v1/store-profile",
     controller: MainController,
     action: "storesAll"
 },
 {
     method: "get",
-    route: "/store-profile/byUserId/:id",
+    route: "/api/v1/store-profile/byUserId/:id",
     controller: MainController,
     action: "storesOneByUserId"
 },
 {
     method: "get",
-    route: "/store-profile/byId/:id",
+    route: "/api/v1/store-profile/byId/:id",
     controller: MainController,
     action: "storesOne"
 },
