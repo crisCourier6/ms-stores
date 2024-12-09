d3afc3b42d12cfaf1e7278aeadf243bf4880edcb
Cristobal Vasquez <cristobal.vasquez.m@usach.cl>
2024-12-09 14:54:23 -0300

    replaced rabbitmq channel with env variable

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import { StoreProfileController } from "./controller/StoreProfileController"
 import { FoodLocalController } from "./controller/FoodLocalController"
 
 AppDataSource.initialize().then(async () => {
-    amqp.connect('amqps://zqjaujdb:XeTIDvKuWz8bHL5DHdJ9iq6e4CqkfqTh@gull.rmq.cloudamqp.com/zqjaujdb', (error0, connection) => {
+    amqp.connect(process.env.RABBITMQ_URL, (error0, connection) => {
         if(error0){
             throw error0
         }
