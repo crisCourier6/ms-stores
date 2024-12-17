30b2b7d00d0f2e66fa1f00b8d06a441286a48777
Cristobal Vasquez <cristobal.vasquez.m@usach.cl>
2024-12-17 19:39:24 -0300

    fixed user edit logic

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -19,7 +19,7 @@ export class UserController {
         return this.userRepository.findOne({where: {id: createdUser.id}, relations:["storeProfile"]})
     }
     async update(req: any) {
-        const {id, storeProfile, expertProfile, userHasRole, ...user } = req
+        const {id, storeProfile, expertProfile, userHasRole, lostPass, ...user } = req
         if (!id) {
             return "id inválida"
         }
